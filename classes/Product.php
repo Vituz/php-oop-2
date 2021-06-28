95c7701d3d78c843d6e40e2978ced38d6a4a4dbc
Vituz <vtbattaglia@gmail.com>
2021-06-28 19:43:55 +0200

    Include classes on index.php

---
+++ classes/Product.php
@@ -0,0 +1,33 @@
+<?php
+
+class Product
+{
+    public $name;
+    public $price;
+    private $code;
+    public $qty;
+    public $desc;
+
+    function __construct(string $name, float $price, string $code, int $qty, string $desc)
+    {
+        $this->name = $name;
+        $this->price = $price;
+        $this->code = $code;
+        $this->qty = $qty;
+        $this->desc = $desc;
+    }
+
+    public function getCode()
+    {
+        return $this->code;
+    }
+
+    public function setCode($code)
+    {
+        $this->code = $code;
+    }
+}
+
+class Tech extends Product
+{
+}
